7f0a56ca151d62aaa5b499e2186ae69b2558fca9
CornyBot <tomass.vite@gmail.com>
2020-10-29 10:13:56 +0200

    October 28: Styling Forms

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ document.querySelector(".signup form").addEventListener("submit", function(event
     event.preventDefault();
     console.log("Submit was pressed!");
 });
+
+function goToSignUpForm() {
+    document.querySelector(".signup").scrollIntoView();
+}
